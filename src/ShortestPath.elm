8f0a57867244e470c30acd024e72f1a05b848f36
Christian Folie <christian.folie@outlook.com>
2022-03-11 17:59:19 +0100

    cleanup

--- src/ShortestPath.elm
+++ src/ShortestPath.elm
@@ -1,12 +1,12 @@
 module ShortestPath exposing (Itinerary, calculatePath)
 
-import Map exposing (Distance, Location, Map)
+import Map exposing (Distance, Location, Map, Road)
 import PriorityQueue exposing (PriorityQueue)
 import Set exposing (Set)
 
 
-type Journey
-    = Milestone Location (Maybe Journey)
+type Path
+    = Milestone Location (Maybe Path)
 
 
 type alias Itinerary =
@@ -14,13 +14,13 @@ type alias Itinerary =
 
 
 type alias State =
-    { travels : PriorityQueue ( Distance, Journey )
+    { travels : PriorityQueue Journey
     , visited : Set Location
     , map : Map
     }
 
 
-flatten : Journey -> List Location
+flatten : Path -> List Location
 flatten (Milestone current next) =
     current
         :: (case next of
@@ -32,11 +32,25 @@ flatten (Milestone current next) =
            )
 
 
+type alias Journey =
+    ( Distance, Path )
+
+
+beginJourneyAt : Location -> Journey
+beginJourneyAt location =
+    ( 0, Milestone location Nothing )
+
+
+exploreOn : Road -> Journey -> Journey
+exploreOn ( _, to, distance ) ( totalDistance, path ) =
+    ( totalDistance + distance, Milestone to (Just path) )
+
+
 calculatePath : Map -> Location -> Location -> Maybe Itinerary
 calculatePath map start end =
     { travels =
         PriorityQueue.empty Tuple.first
-            |> PriorityQueue.insert ( 0, Milestone start Nothing )
+            |> PriorityQueue.insert (beginJourneyAt start)
     , visited = Set.empty
     , map = map
     }
@@ -45,62 +59,45 @@ calculatePath map start end =
         |> Maybe.map List.reverse
 
 
-calculateShortestPath : Location -> State -> Maybe Journey
+calculateShortestPath : Location -> State -> Maybe Path
 calculateShortestPath end state =
-    case state.travels |> PriorityQueue.head of
-        Nothing ->
-            Nothing
-
-        Just ( totalDistance, milestone ) ->
-            let
-                (Milestone currentLocation _ ) = milestone
-            in if state.visited |> Set.member currentLocation then
-                calculateShortestPath end { state | travels = state.travels |> PriorityQueue.tail }
-
-            else if currentLocation == end then
-                Just milestone
-
-            else
+    state.travels
+        |> PriorityQueue.head
+        |> Maybe.andThen
+            (\journey ->
                 let
-                    visited =
-                        state.visited
-                            |> Set.insert currentLocation
+                    ( _, milestone ) =
+                        journey
 
-                    travels =
-                        Map.roadsFrom state.map currentLocation
-                            |> List.map
-                                (\( _, to, distance ) ->
-                                    ( totalDistance + distance, Milestone to (Just milestone) )
-                                )
-                            |> List.foldl
-                                (\item queue ->
-                                    queue |> PriorityQueue.insert item
-                                )
-                                (state.travels |> PriorityQueue.tail)
+                    (Milestone currentLocation _) =
+                        milestone
                 in
-                calculateShortestPath
-                    end
-                    { state
-                        | travels = travels
-                        , visited = visited
-                    }
+                if state.visited |> Set.member currentLocation then
+                    calculateShortestPath end { state | travels = state.travels |> PriorityQueue.tail }
 
+                else if currentLocation == end then
+                    Just milestone
 
+                else
+                    let
+                        visited =
+                            state.visited
+                                |> Set.insert currentLocation
 
--- class Milestone:
---     location: str
---     previous: Milestone
--- # start the travels
--- travels.put((0, new Milestone(start, None)))
--- while not travels.empty():
---   (distance, milestone) = travels.get_next()
---   if milestone.location in visited:
---     continue #skip
---   if milestone.location == end:
---     print(f"we arrived to {end}!")
---     # traverse the event to the origin
---     return
---   visited.append(milestone.location)
---   for road in road_map[milestone.location]:
---     distance_at = distance + road.length
---     travels.put((distance_at, new Milestone(road.destination, milestone)))
+                        travels =
+                            Map.roadsFrom state.map currentLocation
+                                |> List.map
+                                    (\road -> exploreOn road journey)
+                                |> List.foldl
+                                    (\item queue ->
+                                        queue |> PriorityQueue.insert item
+                                    )
+                                    (state.travels |> PriorityQueue.tail)
+                    in
+                    calculateShortestPath
+                        end
+                        { state
+                            | travels = travels
+                            , visited = visited
+                        }
+            )
